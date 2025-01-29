4e34b8c5357d0d2e13b560c1bd3fcd4ff8f6a6a0
Kenny, Orlando Antonio <okenny3@gatech.edu>
2025-01-29 09:20:58 -0500

    excellent

--- src/pages/Special.js
+++ src/pages/Special.js
@@ -9,6 +9,9 @@ import { AnimationMixer } from 'three';
 import {create} from 'zustand';
 import heart from '../3dmodels/heart_compressed.glb'
 import dLogo from '../3dmodels/color.glb'
+import backgroundMusic from '../music/ambiance.mp3'; // Import your audio file
+import meat from '../log2.png'
+
 
 
 
@@ -20,10 +23,43 @@ function Special() {
   const pointer = new THREE.Vector2();
   const raycaster = new THREE.Raycaster();
   const meshRef0 = useRef();
+
+  // Audio ref
+  const audioRef = useRef(new Audio(backgroundMusic)); // Create audio element
+  const [audioReady, setAudioReady] = useState(false);
+
+  function handleAlertAndPlay () {
+ 
+    const audio = audioRef.current;
+    audio.loop = true;
+    audio.play().catch((error) => console.error('Error playing audio:', error)); // Play audio and catch errors
+    
+  };
+
+  
+
+  useEffect(() => {
+    // Play the audio as soon as the component is loaded
+    // Display an alert as a user interaction
+
+
+  
+
+  }, [])
+
+  useEffect(() => {
+    // Play the audio as soon as the component is loaded
+    // Display an alert as a user interaction
+   
+  }, [audioReady])
+  
+  
  
   function Model({ meshReference, name, url, scale, position, rotation }) {
     const { scene, animations } = useGLTF(url);
     const mixer = useRef();
+
+    
   
     useEffect(() => {
       if (scene) {
@@ -69,44 +105,63 @@ function Special() {
 
 
   return (
-   
-        <ARCanvas
-      gl={{ antialias: false, powerPreference: "default", physicallyCorrectLights: true }}
-      onCameraStreamReady={() => console.log("Camera stream ready")}
-      onCameraStreamError={() => console.error("Camera stream error")}
-      onCreated={({ gl, camera }) => {
-        gl.setSize(window.innerWidth, window.innerHeight);
-        cameraRef.current = camera;
-        rendererRef.current = gl;
-      }}
-    >
-   
-
-     
+    <div>
 
 
-           <ARMarker
-        params={{ smooth: true }}
-        type={"barcode"}
-        barcodeValue={0}
-        matrixCodeType="3x3"
-        onMarkerFound={() => {
-          //console.log("Marker Found");
+      
+      {audioReady ? <ARCanvas
+        gl={{ antialias: false, powerPreference: "default", physicallyCorrectLights: true }}
+        onCameraStreamReady={() => console.log("Camera stream ready")}
+        onCameraStreamError={() => console.error("Camera stream error")}
+        onCreated={({ gl, camera }) => {
+          gl.setSize(window.innerWidth, window.innerHeight);
+          cameraRef.current = camera;
+          rendererRef.current = gl;
         }}
       >
-        <ambientLight intensity={1.5} />
-        <directionalLight position={[5, 5, 5]} intensity={10} />
-        
+   
+           <ARMarker
+              params={{ smooth: true }}
+              type={"barcode"}
+              barcodeValue={0}
+              matrixCodeType="3x3"
+              onMarkerFound={() => {
+                //console.log("Marker Found");
+              }}
+            >
+              <ambientLight intensity={1.5} />
+              <directionalLight position={[5, 5, 5]} intensity={10} />
+              
 
-    <Model  url={heart} scale={0.25} position={[0, 0, 0]}/>
-    <Model  url={dLogo} scale={5} position={[0, 1.8, 0]}/>
+              <Model  url={heart} scale={0.25} position={[0, 0, 0]}/>
+              <Model  url={dLogo} scale={5} position={[0, 1.8, 0]}/>
 
 
-          </ARMarker>
+            </ARMarker>
 
 
-    </ARCanvas>
+      </ARCanvas>
+      
+    : 
 
+    <div className='parent'>
+     
+        <div className='test'>
+             <div className="App" id="parentContainer" style={{height: '90vh'}}>
+                <img src={meat} className='meat'></img>
+                <br></br>
+
+                <button type="submit" className='submit' onClick={() => {
+                   setAudioReady(true);
+                   handleAlertAndPlay();
+                    }}>
+                    <b>enter experience</b>
+                </button>
+
+            </div></div></div>}
+
+
+    </div>
 
 
     
